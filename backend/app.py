55c8ce28698c89d0a44a87cfdc240d8c0e0cf7bd
Sujeet Pawar <166638854+Sujeet-Pawar@users.noreply.github.com>
2025-04-17 19:51:38 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__, static_folder="static", template_folder="C:\Users\pawar\OneDrive\Documents\Desktop\project-root\templates")
 
 @app.route("/")
 def home():
